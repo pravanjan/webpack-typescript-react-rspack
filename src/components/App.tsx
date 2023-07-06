2c2c6740fff1340ae6f118c523542ef0f0f54a32
pravanjan <>
2023-07-06 14:24:52 +0530

    Added config changes for rspack

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import reactLogo from "./../assets/img/react_logo.svg";
 const App = () => (
   <div className="app">
     <h1> World!</h1>
-    <p>Foo hellosdfdsfsddhfdsfh</p>
+    <p>Demo on React with webpack and rspack </p>
     <img src={reactLogo} height="480" />
   </div>
 );
